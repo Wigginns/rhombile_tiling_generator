9919db710a3d626a56db9f7adbb8dbca694e1a86
Wigginns <3180645+Wigginns@users.noreply.github.com>
2019-04-11 16:41:34 -0600

    Rename main script. Add colormind integration function.

--- rhombile_tiling.py
+++ rhombile_tiling.py
@@ -17,18 +17,18 @@ def pil_hex():
         draw.polygon((list(rhombus_points_next3(x,y,n+2)) + [x,y]), fill=next(colors), outline=128)
         draw.polygon((list(rhombus_points_next3(x,y,n+4)) + [x,y]), fill=next(colors), outline=128)
         # draw.polygon(list(hex_points(x,y)), outline=128)
-        cc_x, cc_y = next(coords)
-        current_center = str(cc_x) + "." + str(cc_y)
-        #draw.text((x-10,y), current_center, font = fnt, fill="#000000")
+        # cc_x, cc_y = next(coords)
+        # current_center = str(cc_x) + "." + str(cc_y)
+        # draw.text((x-10,y), current_center, font = fnt, fill="#000000")
 
-    image.save('pil_hexes_pointy.png', 'PNG')
+    image.save('megacolor.png', 'PNG')
 
 
 def pil_hex_tricolor():
     image = Image.new("RGBA", (IMAGE_WIDTH,IMAGE_HEIGHT), (0,0,0,0))
-    colors = color_generator()
+
     coords = hex_centers_grid()
-    fnt =ImageFont.load_default().font
+    # fnt =ImageFont.load_default().font
     draw = ImageDraw.Draw(image)
     for x,y in hex_centers():
         #draw.polygon(list(hex_points(x,y)), fill=next(colors), outline=128)
@@ -37,13 +37,38 @@ def pil_hex_tricolor():
         draw.polygon((list(rhombus_points_next3(x,y,n+2)) + [x,y]), fill=color_generator_n(n+2), outline=128)
         draw.polygon((list(rhombus_points_next3(x,y,n+4)) + [x,y]), fill=color_generator_n(n+4), outline=128)
         # draw.polygon(list(hex_points(x,y)), outline=255)
-        cc_x, cc_y = next(coords)
-        current_center = str(cc_x) + "." + str(cc_y)
-        #draw.text((x-10,y), current_center, font = fnt, fill="#000000")
+        # cc_x, cc_y = next(coords)
+        # current_center = str(cc_x) + "." + str(cc_y)
+        # draw.text((x-10,y), current_center, font = fnt, fill="#000000")
 
-    image.save('pil_hexes_tricolor_pointy.png', 'PNG')
+    image.save('tricolor.png', 'PNG')
+
+def pil_hex_colormind():
+    data = '{"model":"default"}'
+    response = requests.post('http://colormind.io/api/', data=data)
+
+    json_string = response.json() #convert to dict
+
+    colors = list()
+    for rgb in json_string['result']:
+        colors.append(tuple(rgb))
+
+    image = Image.new("RGBA", (IMAGE_WIDTH,IMAGE_HEIGHT), (0,0,0,0))
+    
+    draw = ImageDraw.Draw(image)
+    for x,y in hex_centers():
+        n = random.randint(0,5)
+        draw.polygon((list(rhombus_points_next3(x,y,n)) + [x,y]), fill=colors[n%3], outline=128)
+        draw.polygon((list(rhombus_points_next3(x,y,n+2)) + [x,y]), fill=colors[(n+2)%3], outline=128)
+        draw.polygon((list(rhombus_points_next3(x,y,n+4)) + [x,y]), fill=colors[(n+4)%3], outline=128)
+    
+    image.save('colormind.png', 'PNG')
+
+
 
 sys.stdout.write("hello from Python %s\n" % (sys.version,))
 
+
 pil_hex()
 pil_hex_tricolor()
+pil_hex_colormind()
